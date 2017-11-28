3b0d0e3fe3541903f1ce77cf7be0166eeaabec3e
Andrew Ligon <andrew.ligon@nextthought.com>
2017-11-27 22:25:56 -0600

    read search term from the data

--- src/stores/SearchablePagedStore.js
+++ src/stores/SearchablePagedStore.js
@@ -27,7 +27,7 @@ export default class SearchablePagedStore extends SimpleStore {
 		this.set('loadNextPage', null);
 
 		try {
-			const {items, loadNext} = await (this.searchTerm ? this.loadSearchTerm(this.searchTerm) : this.loadInitial());
+			const {items, loadNext} = await (this.get('searchTerm') ? this.loadSearchTerm(this.searchTerm) : this.loadInitial());
 
 			this.set('items', items);
 			this.set('loadNextPage', loadNext);
