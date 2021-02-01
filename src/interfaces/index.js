6fd5da0e5f454be3bc25cbe06957aa667c52988e
Andrew Ligon <andrew.ligon@nextthought.com>
2021-02-01 14:25:23 -0600

    export all the mixins as interfaces too

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -1,1 +1,27 @@
-export { default as Stateful } from './Stateful';
+import * as Mixins from '../mixins';
+
+import createInterface from './create-interface';
+import Stateful from './Stateful';
+
+function convertMixin (mixin) {
+	return createInterface(
+		Object.defineProperties({
+			initInterface (...args) {
+				if (mixin.initMixin) {
+					return mixin.initMixin.apply(this, args);
+				}
+			}
+		}, Object.getOwnPropertyDescriptors(mixin))
+	);
+}
+
+const Interfaces = {
+	Stateful
+};
+
+for (let [key, mixin] of Object.entries(Mixins)) {
+	if (Interfaces[key]) { continue; }
+	Interfaces[key] = convertMixin(mixin);
+}
+
+export default Interfaces;
